15dd8956c15270b969bdd0a3c852b7d6b7a98d09
Benjamin Herson <benjaminherson@gmail.com>
2017-02-22 21:25:39 -0800

    init project, with basic UI component and text

---
+++ Assets/TextController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello World";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			text.text = "Great Scott! You have just awoken in an attic, and you don't rememember how you got here. " +
+						"You try calling for help, but no one answers. You try the door: locked! " +
+						"What are you going to do? You see three boxes: Red, Yellow, and Green.\n\n" +
+						"Choose which one to open by typing R, Y, or G.";
+		}
+	}
+}
